32b4ea94a46804f06ba2beabede8dd80cfd52423
ClownFish <osgochina@gmail.com>
2015-01-14 18:34:47 +0800

    完善流程

--- src/app/Squire_Gearman.class.php
+++ src/app/Squire_Gearman.class.php
@@ -6,17 +6,43 @@
  * Time: 下午3:44
  */
 
-class Squire_Gearman    implements Squire_Common
+class Squire_Gearman
 {
-    static public $worker;
-    public function run($worker,$task)
+    static public $conf_file;
+    static $process_name_prefix = "lzm_squire";
+    static $pid;
+
+
+    static public function register_signal()
     {
-        self::$worker = $worker;
-        $_SERVER["argv"][1] = preg_replace("/\d{1,}\_/","",$task["name"]);
-        sleep(10);
+        swoole_process::signal(SIGUSR1, function ($signal_num) {
+            echo "收到结束信号，结束进程";
+            exit();
+        }
+        );
+    }
+    static public function set_process_name($name)
+    {
+        swoole_set_process_name(self::$process_name_prefix.$name."_".self::$pid);
+    }
+    static public function run($name)
+    {
+        self::set_process_name($name);
+        self::register_signal();
+        $_SERVER["argv"][1] = preg_replace("/\d{1,}\_/","",$name);
+        echo $_SERVER["argv"][1]."\n";
+        sleep(100);
     }
     static public function _exit()
     {
         self::$worker->exit(1);
     }
 }
+
+
+if(!empty($argv[2]))
+    Squire_Gearman::$pid = $argv[2];
+if(!empty($argv[3]))
+    Squire_Gearman::$conf_file = $argv[3];
+
+Squire_Gearman::run($argv[1]);
--- src/include/Squire_Slaver.class.php
+++ src/include/Squire_Slaver.class.php
@@ -7,60 +7,23 @@
  */
 class Squire_Slaver{
 
-    private $process_name_prefix = "lzm_gearman_";
     public $worker;
     public $name;
+    public $pid;
     public $data;
-
-
-
-    private function register_signal()
-    {
-        swoole_process::signal(SIGTERM, function ($signal_num) {
-            $this->worker->write($this->name.":signal call = $signal_num");
-            $this->worker->exit(0);
-        }
-        );
-    }
-
-    private function add_event()
-    {
-        swoole_event_add($this->worker->pipe, function ($pipe) {
-            //TODO 这里可以写主进程通知子进程的逻辑
-            $recv = $this->worker->read();
-        }
-        );
-    }
-
-    static protected function autoload($class)
-    {
-        static $_load = array();
-        if(isset($_load[$class])) return true;
-        include(ROOT_PATH . "app" . DS . "Squire_Common.class.php");
-        $file = ROOT_PATH . "app" . DS . $class . ".class.php";
-        if (file_exists($file)) {
-            include($file);
-        } else {
-            Main::log_write("处理类不存在");
-        }
-        $_load[$class] = true;
-        return true;
-    }
-
     public function run($worker)
     {
-        $GLOBALS["worker"] = $worker;
         $this->worker = $worker;
-        $pid = $this->worker->read();
-        $this->worker->name($this->process_name_prefix.$this->name."_".$pid);
-        $this->register_signal();
-        $this->add_event();
+        $this->pid = $this->worker->read();
         $class = "Squire_".$this->data["parse"];
-        self::autoload($class);
-        (new $class)->run($worker,array_merge($this->data["data"],array("name"=>$this->name)));
+        $this->exec($class);
     }
-     public function exec()
+     public function exec($class)
      {
-
+         $file = ROOT_PATH."app".DS.$class.".class.php";
+         if (!file_exists($file)) {
+             return false;
+         }
+         $this->worker->exec($_SERVER["_"],array($file,$this->name,$this->pid));
      }
 }
